eaf022b61eca95949fda22bb0fe129d09d9c3196
vcjr <victorrcrispinjr@gmail.com>
2020-10-20 19:10:55 -0400

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import drone from "./drone.png";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to DroneChop!</h1>
+      <img src={drone} className="App-logo" alt="logo" />
+    </>
+  );
+}
+
+export default App;
