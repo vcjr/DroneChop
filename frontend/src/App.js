d63ef4217425e56cf93b239509eef2ca8bc35669
vcjr <victorrcrispinjr@gmail.com>
2020-10-20 19:40:34 -0400

    Update and Install bootstrap css for default styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import drone from "./drone.png";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to DroneChop!</h1>
-      <img src={drone} className="App-logo" alt="logo" />
+      <Header />
+      <Container>
+        <h1>Welcome to DroneChop!</h1>
+        <img src={drone} className="App-logo" alt="logo" />
+      </Container>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      Footer Section
+    </footer>
+  )
+}
+
+export default Footer
