69bf8ed7d3f99fe718a68f82ff255f6fe77b6b2f
vcjr <victorrcrispinjr@gmail.com>
2020-10-20 23:02:36 -0400

    Update file structure

---
+++ backend/data/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    _id: "1",
+    name: "DJI Mavic Air 2",
+    image: "/images/dji-mavic-air-2.jpg",
+    description:
+      "Mavic Air 2 takes camera and flight performance to the next level with 48MP photos, 4K/60fps video, up to 34 minutes of flight time, and more.",
+    brand: "DJI",
+    category: "Drones",
+    price: 799.0,
+    countInStock: 115,
+    rating: 4.5,
+    numReviews: 43,
+  },
+  {
+    _id: "2",
+    name: "DJI FPV Goggles",
+    image: "/images/dji-fpv-goggles.jpg",
+    description:
+      "High definition, low latency, strong immersion, long-distance transmission, and strong anti-interference.",
+    brand: "DJI",
+    category: "FPV Goggles",
+    price: 529.0,
+    countInStock: 23,
+    rating: 4,
+    numReviews: 32,
+  },
+  {
+    _id: "3",
+    name: "GoPro HERO8 Black 4K Waterproof Action Camera",
+    image: "/images/gopro-hero8-black-4k.jpg",
+    description:
+      "Beyond Next Level. With unshakable HyperSmooth 2.0 stabilization and built-in mounting, 8 is the one.",
+    brand: "GoPro",
+    category: "HD Action Camera",
+    price: 399.99,
+    countInStock: 46,
+    rating: 5,
+    numReviews: 5,
+  },
+  {
+    _id: "4",
+    name: "DJI Mavic Intelligent Flight Battery",
+    image: "/images/dji-mavic-intelligent-flight-battery.jpg",
+    description:
+      "Intelligent Flight Battery (3,830mAh/11.4V) specially designed for the Mavic. Built-in sensors and bright LEDs tell you the status and remaining power of your battery in real time. Charge with Mavic Battery Charger or Mavic Battery Charging Hub.",
+    brand: "DJI",
+    category: "Part & Accessories",
+    price: 109.0,
+    countInStock: 24,
+    rating: 4.5,
+    numReviews: 34,
+  },
+];
+
+export default products;
