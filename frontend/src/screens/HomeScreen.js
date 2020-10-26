7e2b555f101203046bf4cb64585863835f1a4445
vcjr <victorrcrispinjr@gmail.com>
2020-10-26 16:59:35 -0400

    connect back end routes to front end with proxy, and installing axios for http requests

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap';
-import Product from '../components/Product'
-import products from '../products';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+// import products from '../products';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Here we will fetch all of our products
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+
+      setProducts(data);
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
-        { products.map(product => (
+        {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             {/* <h3>{product.name}</h3> */}
-            <Product product={product}/>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
